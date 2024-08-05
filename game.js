0055dcd6e152a6f65643e8ed46f6656d1bb34975
waychel-l <160815005+waychel-l@users.noreply.github.com>
2024-08-05 15:07:23 +0800

    Set up homescreen and link index.html with game.js
    Update index.html with homescreen and containers
    Update game.js with function to start game

---
+++ game.js
@@ -0,0 +1,12 @@
+let currentState = 1;
+
+function startGame() {
+    document.getElementById('homescreen').style.display = 'none';
+    document.querySelector('.start-button').style.display = 'none';
+    document.getElementById('game-container').style.display = 'block';
+    renderState(currentState);
+}
+
+window.onload = () => {
+    renderState(currentState);
+}
