6cd2fc07db7012c0d6a185ae69f8a4257a5f35bb
waychel-l <160815005+waychel-l@users.noreply.github.com>
2024-08-14 18:10:06 +0800

    Add text wrap
    Add text wrap, unfortunately overrides line break attribute
    Start working on including forms for open-ended, not ready yet so commented out

--- game.js
+++ game.js
@@ -2,13 +2,25 @@ const endings = {
 
 };
 
+function getUserName() {
+    var yourName = document.getElementById('yourName').value;
+    var result = document.getElementById('result');
+    
+    if (nameField.length < 3) {
+        result.textContent = 'Username must contain at least 3 characters';
+        //alert('Username must contain at least 3 characters');
+    } else {
+        result.textContent = 'Your username is: ' + nameField;
+        //alert(nameField);
+    }
+    }
 
 const gameData = {
     "1": {
-        "text": "You awake to un-life, a strange inverted non-existence. You hear no sounds, just a resounding silence. You have no eyes, and there is no light, but you see the void stretch ahead of you endlessly." + "\r\n" + "\r\n" + "A thought presents itself: who was I?",
+        "text": "You awake to un-life, a strange inverted non-existence. You hear no sounds, just a resounding silence. You have no eyes, and there is no light, but you see the void stretch ahead of you endlessly." + "\r\n" + "\r\n" + "A thought presents itself: “who was I?”",
         "image": "images/placeholder.jpg",
         "choices": {
-            "Placeholder 1": [2],
+            "“So that was my name.”": [2],
         }
     },
 
@@ -34,7 +46,7 @@ const gameData = {
         "text": "They start to mumble louder and faster, words spilling out of them almost too quickly for you to catch (after all you are a moth, with a little moth brain): the events that followed your passing, the things they never got to say to you or do with you, how they missed you. Then their speech starts to slow, weighed down with doubt; it becomes a question, a plea." + "\r\n" + "\r\n" + "They hold up four fingers–each one represents one option for what to do with your body." + "\r\n" + "\r\n" + "“yourName, if it’s really you… give me a sign. What would you want me to do?”",
         "image": "images/placeholder.jpg",
         "choices": {
-            "Bury you in a biodegradable shroud under the roots of a tree that you used to visit often with your dearest friends.": [5],
+            "Bury you in a biodegradable shroud under the roots of a tree that you used to visit with your dearest friends.": [5],
             "Cremate you and keep the urn in your family home, beside other family members who passed before you.": [5],
             "Donate your body to a lab that dissects people to discover where sentience is located in the human body." : [5],
             "Encase your body in a very hard material and launch it into outer space.": [5],
@@ -78,8 +90,7 @@ function renderState(state) {
     const storyText = document.getElementById('story-text');
     const storyImage = document.getElementById('story-image');
     const choicesContainer = document.getElementById('choices');
-
-    console.log('renderState Function called.');
+    //const formContainer = 
 
     const img = new Image();
     img.src = gameData[state].image;
@@ -88,6 +99,7 @@ function renderState(state) {
         storyImage.src = img.src;
         storyText.textContent = gameData[state].text;
         storyText.setAttribute('style', 'white-space: pre;'); //Allow adding line breaks to text
+        storyText.setAttribute('style', 'overflow-wrap: break-word;'); //Wrap text
         choicesContainer.innerHTML = '';   
 
         for (const [choice, info] of Object.entries(gameData[state].choices)) {
